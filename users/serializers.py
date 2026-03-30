00fc0780136d0d62f8d111f26fb06e1758e7cc53
Tuhin Al Rakib <eng.tuhin77@gmail.com>
2026-03-30 23:05:05 +0600

    user serializers update

--- users/serializers.py
+++ users/serializers.py
@@ -5,10 +5,26 @@ from djoser.serializers import UserSerializer as BaseUserSerializers
 class UserCreateSerializer(BaseUserCreateSerrializers):
     class Meta(BaseUserCreateSerrializers.Meta):
         ref_name = "CustomUserCreateSerializer"
-        fields = ['id', "email", 'password', 'first_name', "last_name", "address", "phone_number"]
+        fields = [
+            'id',
+            "email",
+            'password',
+            'first_name',
+            "last_name",
+            "address",
+            "phone_number"
+        ]
 
 
 class UserSerializer(BaseUserSerializers):
     class Meta(BaseUserSerializers.Meta):
         ref_name = "CustomUserSerializer"
-        fields = ["email", 'first_name', "last_name", "address", "phone_number"]
+        fields = [
+            "email",
+            'first_name',
+            "last_name",
+            "address",
+            "phone_number",
+            "is_staff",
+            "is_active",
+        ]
