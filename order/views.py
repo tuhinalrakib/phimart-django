6b0c7c96fa6288149699f11501487ca03cb99245
Tuhin Al Rakib <eng.tuhin77@gmail.com>
2026-03-31 20:55:53 +0600

    create cart view edit

--- order/views.py
+++ order/views.py
@@ -7,6 +7,7 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.decorators import action
 from order.services import OrderServices
 from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 class CartViewSet(CreateModelMixin, RetrieveModelMixin , DestroyModelMixin, GenericViewSet):
@@ -18,6 +19,15 @@ class CartViewSet(CreateModelMixin, RetrieveModelMixin , DestroyModelMixin, Gene
     
     def get_queryset(self):
         return Cart.objects.prefetch_related("items__product").filter(user = self.request.user)
+    
+    def create(self, request, *args, **kwargs):
+        existing_cart = Cart.objects.filter(user = request.user).first()
+        
+        if existing_cart:
+            serializer = self.get_serializer(existing_cart)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return super().create(request, *args, **kwargs)
     
 class CartItemViewSet(ModelViewSet):
     http_method_names = ["get", "post", "patch", "delete"]
